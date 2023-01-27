bf762a4436f3de271458713ddea737edd834a792
vinylautomation <49808124+vinayaktule@users.noreply.github.com>
2023-01-27 07:33:00 +0000

    Fixing package for node

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const app = require('./app')
 const {PORT} = process.env
 
 app.listen(PORT, ()=>{
-    console.log(`SERVER is running on {PORT}`)
+    console.log(`SERVER is running on ${PORT}`)
 })
